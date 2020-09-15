7286bb8b0438c8e35a4d0a924e6376a148c6279c
Jay Joshi <php12@bexcodeservices.com>
2020-09-15 18:44:52 +0530

    15092020_validation issue

--- app/Http/Controllers/Admin/EmployeeController.php
+++ app/Http/Controllers/Admin/EmployeeController.php
@@ -82,12 +82,14 @@ class EmployeeController extends Controller
     public function store(CreateEmployeeRequest $request)
     {
         $role = Config::get('constants.operator');
+        
         // dd($var);
         // exit();
-        // dd($request);    
+        // dd($request);
         //dd($request->file('license_certificates')->getClientOriginalName());
         //dd($request);
         $request->request->add(['role' => $role, 'password'=>Hash::make('12345678')]); 
+        
         $request->merge([
             'location_associated' => json_encode($request->location_associated),
         ]);
