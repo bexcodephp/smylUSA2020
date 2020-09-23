a11dddc5f8cb6160eb150e9c308be17b249b1296
Swami Chirag <php11@bexcodeservices.com>
2020-09-24 00:02:49 +0530

    vimal-23092020 reources

---
+++ resources/views/front/dashboard/patientChat.blade.php
@@ -0,0 +1,27 @@
+<div class="chat-box p-chatbox col p-0">
+    <div class="card">
+        <div class="card-header p-0">
+            <button class="btn btn-primary w-100 btn-lg btn-open-chat" type="button">
+                <span>Patient Name</span><i class="fas fa-circle icon-online"></i>
+            </button>
+        </div>
+        <div class="card-body">
+            <div>
+                <ul class="list-group list-chat">
+                    <li class="list-group-item d-flex text-left flex-column chat-item">
+                        <p class="color-blue w-100 m-0 chat-msg">Patient Message</p>
+                        <small>Your account has been created</small>
+                    </li>
+                    <li class="list-group-item d-flex text-left flex-column chat-item">
+                        <p class="color-gray w-100 m-0 chat-msg">Patient Message</p>
+                        <small>Your account has been created</small>
+                    </li>
+                </ul>
+            </div>
+            <div class="input-group mb-3">
+                <input type="text" class="form-control" placeholder="Recipient's username" aria-label="Recipient's username" aria-describedby="basic-addon2">
+                <button class="input-group-text btn btn-send p-0" type="button"><i class="fa fa-envelope"></i></button>
+            </div>
+        </div>
+    </div>
+</div>
